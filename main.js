9370181e833793392a9077c8244d5bcf651b3e0a
Daniel <danielpellegrini87@gmail.com>
2021-01-13 18:18:11 +0100

    BONUS COMPLETED

--- main.js
+++ main.js
@@ -66,4 +66,20 @@ $(document).ready(function() {
 
   });
 
+  // BONUS
+
+  $('.fa-circle').click(function() {
+
+    $('.active').removeClass('active');
+
+    $('.images img')
+      .eq($(this).index())
+      .addClass('active');
+
+    $('.fa-circle')
+      .eq($(this).index())
+      .addClass('active');
+
+  });
+
 });
